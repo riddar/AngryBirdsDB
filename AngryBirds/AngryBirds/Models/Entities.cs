bc139883e179cada97bb84e4d7447f7f743fb1d7
RIddaR <fredrik.ridderheim@hotmail.com>
2017-12-01 13:49:33 +0100

    Entities Added

---
+++ AngryBirds/AngryBirds/Models/Entities.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AngryBirds.Models
+{
+    class Entities
+    {
+
+    }
+
+    public class Player
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+    }
+
+    public class Level
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(10)]
+        public int Birds { get; set; }
+    }
+
+    public class Score
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(10)]
+        public string Points { get; set; }
+
+        public int PlayerId { get; set; }
+        [ForeignKey("PlayerId")]
+        public virtual Player Player { get; set; }
+
+        public int LevelId { get; set; }
+        [ForeignKey("LevelId")]
+        public virtual Level Level { get; set; }
+
+    }
+}
---
+++ AngryBirds/AngryBirds/Program.cs
@@ -0,0 +1,30 @@
+﻿using AngryBirds.Models;
+using System.Data.Entity;
+
+namespace AngryBirds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+
+    public class AngryBirdsContext : DbContext
+    {
+        public const string ConnectionString = (@"Data Source=(localdb)\MSSQLLocalDB;
+                                                  Initial Catalog = AngryBirds; 
+                                                  Integrated Security = True; 
+                                                  Connect Timeout = 30; 
+                                                  Encrypt=False;
+                                                  TrustServerCertificate=True;
+                                                  ApplicationIntent=ReadWrite;
+                                                  MultiSubnetFailover=False");
+        public AngryBirdsContext() : base(ConnectionString) { }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Level> Levels { get; set; }
+        public DbSet<Score> Score { get; set; }
+    }
+}
