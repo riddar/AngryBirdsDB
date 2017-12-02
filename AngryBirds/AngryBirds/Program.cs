7cb4c1ac071c90710d883c3ad575e4872565b72e
RIddaR <fredrik.ridderheim@hotmail.com>
2017-12-02 14:36:24 +0100

    ScoreController Added + view

--- AngryBirds/AngryBirds/Program.cs
+++ AngryBirds/AngryBirds/Program.cs
@@ -1,12 +1,63 @@
 ﻿using AngryBirds.Models;
 using System.Data.Entity;
+using System.Runtime.Remoting.Contexts;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System;
 
 namespace AngryBirds
 {
     class Program
     {
+        AngryBirdsContext context = new AngryBirdsContext();
+
         static void Main(string[] args)
         {
+            
+
+            
+
+            
+
+            
+        }
+
+        public IQueryable<Score> GetAllScores()
+        {
+            try
+            {
+                var scores = from score in context.Scores
+                             select score;
+                return scores;
+            }
+            catch(Exception e){
+                throw e;
+            }       
+        }
+
+        public void AddPlayerScoreByLevelId(int points, int playerId, int levelId)
+        {
+            Score score = new Score {Points = points, PlayerId = playerId, LevelId = levelId};
+
+            try
+            {
+                context.Scores.insertOnSubmit(score);
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void UpdateScoreById(int id)
+        {
+
+        }
+
+        public void DeleteScoreById(int id)
+        {
+
         }
     }
 
@@ -25,6 +76,6 @@ namespace AngryBirds
         public AngryBirdsContext() : base(ConnectionString) { }
         public DbSet<Player> Players { get; set; }
         public DbSet<Level> Levels { get; set; }
-        public DbSet<Score> Score { get; set; }
+        public DbSet<Score> Scores { get; set; }
     }
 }
