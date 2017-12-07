8b720e4051c82ce947efcb1bdef568da87f82a37
RIddaR <fredrik.ridderheim@hotmail.com>
2017-12-07 09:14:52 +0100

    Add new Seed after mergeFail

--- AngryBirds/AngryBirds/Migrations/Configuration.cs
+++ AngryBirds/AngryBirds/Migrations/Configuration.cs
@@ -18,26 +18,33 @@ namespace AngryBirds.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+            Models.Player player1 = new Models.Player() { Id = 1, Name = "Pontus Magnusson" };
+            Models.Player player2 = new Models.Player() { Id = 2, Name = "Fredrik Ridderheim" };
+            Models.Player player3 = new Models.Player() { Id = 3, Name = "Nisse Nilsson" };
+
+            Models.Level level1 = new Models.Level() { Id = 1, Name = "Level 1", Birds = 4 };
+            Models.Level level2 = new Models.Level() { Id = 2, Name = "Level 2", Birds = 3 };
+            Models.Level level3 = new Models.Level() { Id = 3, Name = "Level 3", Birds = 5 };
+            
 
             context.Players.AddOrUpdate(x => x.Id,
-                new Models.Player() { Id = 1, Name = "Pontus Magnusson" },
-                new Models.Player() { Id = 2, Name = "Fredrik Ridderheim" },
-                new Models.Player() { Id = 3, Name = "Nisse Nilsson" }
+                player1,
+                player2,
+                player3
                 );
 
             context.Levels.AddOrUpdate(x => x.Id,
-                new Models.Level() { Id = 1, Name = "Level 1", Birds = 4 },
-                new Models.Level() { Id = 2, Name = "Level 2", Birds = 3 },
-                new Models.Level() { Id = 3, Name = "Level 3", Birds = 5 }
+                level1,
+                level2,
+                level3
                 );
-            
 
             context.Score.AddOrUpdate(x => x.Id,
-                new Models.Score() { Id = 1, Level = context.Levels.Where(l => l.Id == 1).First(), Player = context.Players.Where(p => p.Id == 1).First(), Points = 3 },
-                new Models.Score() { Id = 2, Level = context.Levels.Where(l => l.Id == 2).First(), Player = context.Players.Where(p => p.Id == 1).First(), Points = 2 },
-                new Models.Score() { Id = 3, Level = context.Levels.Where(l => l.Id == 1).First(), Player = context.Players.Where(p => p.Id == 2).First(), Points = 4 },
-                new Models.Score() { Id = 4, Level = context.Levels.Where(l => l.Id == 3).First(), Player = context.Players.Where(p => p.Id == 3).First(), Points = 5 },
-                new Models.Score() { Id = 5, Level = context.Levels.Where(l => l.Id == 2).First(), Player = context.Players.Where(p => p.Id == 3).First(), Points = 2 }
+                new Models.Score() { Id = 1, Level = level1, Player = player1, Points = 3 },
+                new Models.Score() { Id = 2, Level = level2, Player = player1, Points = 2 },
+                new Models.Score() { Id = 3, Level = level1, Player = player2, Points = 4 },
+                new Models.Score() { Id = 4, Level = level3, Player = player3, Points = 5 },
+                new Models.Score() { Id = 5, Level = level2, Player = player3, Points = 2 }
                 );
         }
     }
